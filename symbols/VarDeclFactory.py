077ee325d8733a39b86e14377ecd2eb4b8fa5d59
Sherif Tarek <sherift1552@gmail.com>
2020-04-25 23:58:57 +0200

    add var decl factory

---
+++ symbols/VarDeclFactory.py
@@ -0,0 +1,19 @@
+from .NonTerminalSymbol import NonTerminalSymbol
+from .TerminalFactory import TerminalFactory
+from tokens.tokenizer import Token
+from .TypeSpecFactory import TypeSpecFactory
+from .VarDeclDashFactory import VarDeclDashFactory
+
+class VarDeclFactory:
+	def makeSymbol():
+		typeSpec = TypeSpecFactory.makeSymbol()
+		if typeSpec == None:
+			return None
+
+		ident = TerminalFactory.makeSymbol(Token.ID)
+
+		varDeclDash = VarDeclDashFactory.makeSymbol()
+		if varDeclDash:
+			return None
+
+		return NonTerminalSymbol("VarDecl", typeSpec, ident, varDeclDash)
